ae72ad6e064e5184cb2e8f2ea2d5abdba2eb6884
Nikki A <nikki.apostolakis@gmail.com>
2019-01-27 15:45:36 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -19,7 +19,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
-
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
   end
 
 
