f01cb3edefba07528a2eb590cd820dbbf5be2600
Nikki A <nikki.apostolakis@gmail.com>
2019-01-27 15:49:28 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,9 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(params.require(:student))
+    @student.save
+    redirect_to student_path(@student)
 
   end
 
